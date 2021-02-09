dba2d41e424651e2972153a247ffe6bc33b8fdca
Jmiguel14 <miguel.jurado@epn.edu.ec>
2021-02-09 01:56:30 -0500

    ImageVideo

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -62,9 +62,6 @@ public class MainActivity extends AppCompatActivity {
                 if (user.getImageURL().equals("default")){
                     profile_image.setImageResource(R.mipmap.ic_launcher);
                 } else {
-
-                    //change this
-                    //Glide.with(getApplicationContext()).load(user.getImageURL()).into(profile_image);
                     Picasso.get().load(user.getImageURL()).into(profile_image);
                 }
             }
